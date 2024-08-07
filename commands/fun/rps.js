1d68e78e08d3e1a4b16c38429b020e975a13da4e
Sean <sean.weber4@yahoo.com>
2024-08-06 19:47:20 -0500

    Event Handling and Organization

--- commands/fun/rps.js
+++ commands/fun/rps.js
@@ -1,12 +1,13 @@
 const { SlashCommandBuilder } = require('discord.js');
 const hands = ['rock', 'paper', 'scissors'];
-const hand = hands[Math.floor(Math.random() * hands.length)];
+
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('rps')
 		.setDescription('plays rock paper scissors'),
 	async execute(interaction) {
+		const hand = hands[Math.floor(Math.random() * hands.length)];
 		await interaction.reply(`I choose ${hand}!`);
 	},
 };
