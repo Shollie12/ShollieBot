aa4ace83f893279ed266ef4b0daa09a97ca8bee6
Sean <sean.weber4@yahoo.com>
2024-08-07 17:27:26 -0500

    Added 'reload' command.

--- commands/fun/rps.js
+++ commands/fun/rps.js
@@ -5,7 +5,7 @@ const hands = ['rock', 'paper', 'scissors'];
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('rps')
-		.setDescription('plays rock paper scissors'),
+		.setDescription('Plays rock, paper, scissors.'),
 	async execute(interaction) {
 		const hand = hands[Math.floor(Math.random() * hands.length)];
 		await interaction.reply(`I choose ${hand}!`);
