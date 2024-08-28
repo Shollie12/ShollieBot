60b82368ba51a0a73d7c981602905edbcbb0a98a
Oliver <oliver.t.weber4@gmail.com>
2024-08-27 17:02:00 -0700

    Nah this didn't work

--- commands/fun/gpt.js
+++ commands/fun/gpt.js
@@ -61,9 +61,7 @@ module.exports = {
     await axios.post(`http://localhost:5001/api/v1/generate`, JSON.stringify(my_json))
       .then(async function (response) {
         // handle success
-        const responseObj = JSON.parse(response)
-        const rawText = responseObj.results[0].text;
-        // const rawText = response.data.results[0].text;
+        const rawText = response.data.results[0].text;
         console.log(`\nThis is the raw text: ${rawText.replace(/\n/g, `\\n`).replace(/\r/g, `\\r`)}`);
         const cleanText = rawText.replace(/^\n/g, ``);
         await interaction.editReply(`> *${userprompt}*\n${cleanText}`);
