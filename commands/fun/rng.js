5623f2cf8e2bb899d0c10d508865ada768a17453
Sean <sean.weber4@yahoo.com>
2024-09-06 16:52:06 -0700

    Added extra interactability with the rng and rps commands

--- commands/fun/rng.js
+++ commands/fun/rng.js
@@ -8,18 +8,33 @@ const axios = require("axios");
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('rng')
-		.setDescription('Replies with a number 1 - 100'),
-	async execute(interaction) {
-        axios.get('http://www.randomnumberapi.com/api/v1.0/random?min=1&max=100&count=1')
-  .then(await function (response) {
-    // handle success
-    console.log(`\n\nThis is the response: ${response.data[0]}\n\n`);
-    interaction.reply(`${response.data[0]}`);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
+		.setDescription('Replies with a number! Default: 1 - 100')
+    .addIntegerOption(option =>
+      option
+            .setName(`maximum`)
+            .setDescription(`Set the maximum value for your random number.`)
+    ),
+	async execute(interaction, maximumOption) {
+    let maximum = 100;
+    if(maximumOption) {
+      maximum = maximumOption;
+    } else if(interaction.options.getInteger(`maximum`)){
+      maximum = interaction.options.getInteger(`maximum`);
+    }
+    
+    if(maximum <= 1){
+      interaction.reply(`Please enter a number greater than one!`);
+    } else {
+      axios.get(`http://www.randomnumberapi.com/api/v1.0/random?min=1&max=${maximum}&count=1`)
+        .then(await function (response) {
+          // handle success
+          interaction.reply(`${response.data[0]}`);
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        })
+    }
 	},
 };
 
--- commands/fun/rps.js
+++ commands/fun/rps.js
@@ -1,13 +1,83 @@
+/*
+The 'rps' command plays a basic game of rock paper scissors with
+the user.
+*/
 const { SlashCommandBuilder } = require('discord.js');
 const hands = ['rock', 'paper', 'scissors'];
-
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('rps')
-		.setDescription('Plays rock, paper, scissors.'),
-	async execute(interaction) {
+		.setDescription('Plays rock, paper, scissors.')
+			.addStringOption(option =>
+				option
+					.setName(`choice`)
+					.setDescription(`Choose either rock, paper, or scissors.`)
+					.setRequired(true)),
+	async execute(interaction, choiceOption) {
+		let choice = choiceOption;
+		if(!choiceOption) {
+			choice = interaction.options.getString(`choice`, true);
+		}
+		choice = choice.toLowerCase().trim();
 		const hand = hands[Math.floor(Math.random() * hands.length)];
-		await interaction.reply(`I choose ${hand}!`);
+		let result = `unknown`;
+
+		switch(choice){
+			case hand:
+				result = `tie`;
+				break;
+			case `rock`:
+				switch(hand){
+					case `paper`:
+						result = `lose`;
+						break;
+					case `scissors`:
+						result = `win`;
+						break;
+				}
+				break;
+			case `paper`:
+				switch(hand){
+					case `scissors`:
+						result = `lose`;
+						break;
+					case `rock`:
+						result = `win`;
+						break;
+				}
+				break;
+			case `scissors`:
+				switch(hand){
+					case `rock`:
+						result = `lose`;
+						break;
+					case `paper`:
+						result = `win`;
+						break;
+				}
+				break;
+			default:
+				result = `unknown`;
+				break;
+		}
+
+		switch(result){
+			case `unknown`:
+				await interaction.reply(`You chose ${choice} and I choose ${hand}! :thinking: I'm not sure who wins here...`);
+				break;
+			case `win`:
+				await interaction.reply(`You chose ${choice} and I choose ${hand}! :tada: You win! :tada:`);
+				break;
+			case `lose`:
+				await interaction.reply(`You chose ${choice} and I choose ${hand}! You lose.`);
+				break;
+			case `tie`:
+				await interaction.reply(`You chose ${choice} and I choose ${hand}! We tied!`);
+				break;
+			default:
+				await interaction.reply(`There was an error running this command.`);
+				break;
+		}	
 	},
 };
